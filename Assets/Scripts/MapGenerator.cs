729b834562b1ed8bbff3d258f4bbf449fbbafbef
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2024-11-08 15:12:00 -0400

    Added player tile with player movement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -11,9 +11,10 @@ public class MapGenerator : MonoBehaviour
 {
     // References.
     public Tilemap map;
-    public Tile borderTile, groundTile, chestTile;
+    public Tilemap playerLayer;
+    public Tile borderTile, groundTile, chestTile, playerTile;
     public Tile[] houseTiles;
-    int randomHouseIndex;
+    
 
     // Variables.
     int chestsToPlace;
@@ -22,12 +23,17 @@ public class MapGenerator : MonoBehaviour
     int housesToPlace = 0;
     int xOffset;
     int yOffset;
+    int playerPosX;
+    int playerPosY;
+    int playerPosition;
     public int rollRange;
     public int houseChance;
+    int randomHouseIndex;
+    Vector3Int currentPosition;
     string path = Application.dataPath + "/TextMaps/map.txt";
 
     // Constants.
-    int maxChests = 4;
+    const int maxChests = 4;
     const int maxHouses = 4;
 
 
@@ -37,18 +43,26 @@ public class MapGenerator : MonoBehaviour
         chestsToPlace = Random.Range(1, maxChests);
         housesToPlace = Random.Range(1, 4);
 
-        // Define random x and y within a range.
-        int randomX = Random.Range(5, 20);
-        int randomY = Random.Range(5, 10);
-
+        // Generate the map string.
         string Textmap = GenerateMapString(23, 13);
 
+        // Convert string to tilemap.
         ConvertMapToTilemap(Textmap);
+
+        // Spawn the player at initial position.
+        SpawnPlayer();
+    }
+
+    private void Update()
+    {
+        // Gather input from the player.
+        HandleInput();
     }
 
     string GenerateMapString(int width, int height)
     {
         char[,] map = new char[width, height];
+
 
         // Define rules.
         char wall = '#';
@@ -56,8 +70,7 @@ public class MapGenerator : MonoBehaviour
         char chestTile = '$';
         char groundTile = '_';
 
-        int randomX = Random.Range(3, width - 3);
-        int randomY = Random.Range(3, height - 3);
+        
         Debug.Log("Houses placed " + housesPlaced);
         // Draw map using symbols.
         for (int y = 0; y < height; y++)
@@ -65,14 +78,6 @@ public class MapGenerator : MonoBehaviour
             for (int x = 0; x < width; x++)
             {
                 map[x, y] = groundTile;
-
-
-                if (x == randomX && y == randomY)
-                {
-                    map[x, y] = houseTile;
-                    housesPlaced++;
-                }
-
 
                 if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
                 {
@@ -111,15 +116,12 @@ public class MapGenerator : MonoBehaviour
                         map[x, y] = groundTile;
                     }
                 }
+      
                 else
                 {
                     // Draw the ground
                     map[x, y] = groundTile;
                 }
-
-
-
-
             }
         }
 
@@ -134,6 +136,51 @@ public class MapGenerator : MonoBehaviour
             mapBuilder.AppendLine();
         }
         return mapBuilder.ToString();
+    }
+
+    void SpawnPlayer()
+    {
+        playerPosX = map.cellBounds.xMin + map.cellBounds.size.x / 2;
+        playerPosY = map.cellBounds.yMin + map.cellBounds.size.y / 2;
+        currentPosition = new Vector3Int(playerPosX, playerPosY, 0);
+        playerLayer.SetTile(currentPosition, playerTile);
+        
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up arrow pressed");
+            playerLayer.SetTile(currentPosition, null);
+            Vector3Int up = new Vector3Int(0, 1, 0);
+            currentPosition += up;
+            playerLayer.SetTile(currentPosition, playerTile);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left arrow pressed");
+            playerLayer.SetTile(currentPosition, null);
+            Vector3Int left = new Vector3Int(-1, 0, 0);
+            currentPosition += left;
+            playerLayer.SetTile(currentPosition, playerTile);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Right arrow pressed");
+            playerLayer.SetTile(currentPosition, null);
+            Vector3Int right = new Vector3Int(1, 0, 0);
+            currentPosition += right;
+            playerLayer.SetTile(currentPosition, playerTile);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Left arrow pressed");
+            playerLayer.SetTile(currentPosition, null);
+            Vector3Int down = new Vector3Int(0, -1, 0);
+            currentPosition += down;
+            playerLayer.SetTile(currentPosition, playerTile);
+        }
     }
 
     void ConvertMapToTilemap(string mapData)
@@ -162,6 +209,9 @@ public class MapGenerator : MonoBehaviour
                     case '@':
                         randomHouseIndex = Random.Range(0, houseTiles.Length);
                         map.SetTile(tilePosition, houseTiles[randomHouseIndex]);
+                        break;
+                    case 'P':
+                        map.SetTile(tilePosition, playerTile);
                         break;
                 }
             }
